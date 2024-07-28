7aa7ba565b5a1a7908cd6e7005587ca9d5d29248
Abraham Galue <abrahamgalue512@gmail.com>
2024-07-28 15:52:48 -0400

    Middleware added for parsing JSON files coming with Express

--- clase-2/3.express.js
+++ clase-2/3.express.js
@@ -6,6 +6,34 @@ const PORT = process.env.PORT ?? 1234
 const app = express()
 app.disable('x-powered-by')
 
+app.use(express.json())
+
+// app.use((req, res, next) => {
+//   console.log('mi primer middleware')
+//   // trackear la request a la base de datos
+//   // revisar si el usuario tiene cookies
+
+//   if (req.method !== 'POST') return next()
+//   if (req.headers['content-type'] !== 'application/json') return next()
+
+//   let body = ''
+
+//   // escuchar el evento data
+//   req.on('data', chunk => {
+//     body += chunk.toString()
+//   })
+
+//   req.on('end', () => {
+//     const data = JSON.parse(body)
+
+//     data.timestapm = Date.now()
+//     // mutar la request y meter la informacion en el req.body
+//     req.body = data
+//     // la req es unica para cada peticion
+//     next()
+//   })
+// })
+
 app.get('/pokemon/ditto', (req, res) => {
   res.json(ditto)
 })
@@ -15,22 +43,11 @@ app.get('/', (req, res) => {
 })
 
 app.post('/pokemon', (req, res) => {
-  let body = ''
-
-  // escuchar el evento data
-  req.on('data', chunk => {
-    body += chunk.toString()
-  })
-
-  req.on('end', () => {
-    const data = JSON.parse(body)
-
-    data.timestapm = Date.now()
-    res.status(201).json(data)
-  })
+  // req.body deberiamos guardar en la bdd
+  res.status(201).json(req.body)
 })
 
-// La ul9tima a la que va a llegar
+// La ultima a la que va a llegar
 app.use((req, res) => {
   res.status(404).send('<h1>404<h1/>')
 })
