4c7e6f990f90e662b44acef74b36eca9ab9a460c
Abraham Galue <abrahamgalue512@gmail.com>
2024-07-28 14:56:43 -0400

    POST and GET requests migrated to Express

--- clase-2/3.express.js
+++ clase-2/3.express.js
@@ -1,12 +1,33 @@
 const express = require('express')
-const app = express()
-
-app.disable('x-powered-by')
+const ditto = require('./pokemon/ditto.json')
 
 const PORT = process.env.PORT ?? 1234
 
+const app = express()
+app.disable('x-powered-by')
+
+app.get('/pokemon/ditto', (req, res) => {
+  res.json(ditto)
+})
+
 app.get('/', (req, res) => {
   res.send('<h1>MI primer hello world</h1>')
+})
+
+app.post('/pokemon', (req, res) => {
+  let body = ''
+
+  // escuchar el evento data
+  req.on('data', chunk => {
+    body += chunk.toString()
+  })
+
+  req.on('end', () => {
+    const data = JSON.parse(body)
+
+    data.timestapm = Date.now()
+    res.status(201).json(data)
+  })
 })
 
 app.listen(PORT, () => {
