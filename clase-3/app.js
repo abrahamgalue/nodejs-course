bbdc255491c91312e3278bb86dd36500a349ca0d
Abraham Galue <abrahamgalue512@gmail.com>
2024-07-29 16:53:56 -0400

    Added GET method to get the list of movies by genre

--- clase-3/app.js
+++ clase-3/app.js
@@ -9,6 +9,12 @@ app.get('/', (req, res) => {
 })
 
 app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase()))
+
+    return res.json(filteredMovies)
+  }
   res.json(movies)
 })
 
