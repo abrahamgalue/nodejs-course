4012cf5d6f3af6925e2af8cfe9f3661688e66826
Abraham Galue <abrahamgalue512@gmail.com>
2024-07-29 17:02:42 -0400

    Added GET method to search for a movie by its id

--- clase-3/app.js
+++ clase-3/app.js
@@ -18,6 +18,16 @@ app.get('/movies', (req, res) => {
   res.json(movies)
 })
 
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+
+  const movie = movies.find(movie => movie.id === id)
+
+  if (movie) return res.json(movie)
+
+  res.status(404).json({ message: 'Movie not found' })
+})
+
 const PORT = process.env.PORT ?? 1234
 
 app.listen(PORT, () => {
