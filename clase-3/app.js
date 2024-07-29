4946597f3d05f9a8401fb67318145cbeb0fee43a
Abraham Galue <abrahamgalue512@gmail.com>
2024-07-29 16:51:06 -0400

    Added GET method to obtain the list of films

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const movies = require('./movies.json')
 
 const app = express()
 app.disable('x-powered-by')
 
 app.get('/', (req, res) => {
   res.json({ message: 'hola mundo' })
+})
+
+app.get('/movies', (req, res) => {
+  res.json(movies)
 })
 
 const PORT = process.env.PORT ?? 1234
