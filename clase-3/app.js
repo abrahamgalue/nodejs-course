711fefa97e9994dbe278b1d1501ab0588596e6db
Abraham Galue <abrahamgalue512@gmail.com>
2024-07-31 19:35:07 -0400

    Created page and added DELETE method using cors middleware

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,15 +1,40 @@
 const express = require('express')
+const cors = require('cors')
 const crypto = require('node:crypto')
+
 const movies = require('./movies.json')
 const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:1234',
+      'http://localhost:8080',
+      'http://localhost:3000',
+      'https://movies.com'
+    ]
+
+    if (ACCEPTED_ORIGINS.indexOf(origin) !== -1) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}
+))
 app.disable('x-powered-by')
 
-app.get('/', (req, res) => {
-  res.json({ message: 'hola mundo' })
-})
+// Metodos normales: GET/HEAD/POST
+// Metodos complejos: PUT/PATCH/DELETE
+
+// CORS PRE-Flight
+// OPTIONS
 
 app.get('/movies', (req, res) => {
   const { genre } = req.query
@@ -52,6 +77,19 @@ app.post('/movies', (req, res) => {
   res.status(201).json(newMovie) // actualizar la cache del cliente
 })
 
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex < 0) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
+})
+
 app.patch('/movies/:id', (req, res) => {
   const result = validatePartialMovie(req.body)
 
@@ -75,6 +113,20 @@ app.patch('/movies/:id', (req, res) => {
 
   res.json(updateMovie)
 })
+
+// Lo hace el middleware cors
+
+// app.options('/movies/:id', (req, res) => {
+//   const origin = req.header('origin')
+
+//   // Cuando la peticion es del mismo origin
+//   // http://localhost:1234 => http://localhost:1234
+//   if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+//     res.header('Access-Control-Allow-Origin', origin)
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+//   }
+//   res.sendStatus(200)
+// })
 
 const PORT = process.env.PORT ?? 1234
 
