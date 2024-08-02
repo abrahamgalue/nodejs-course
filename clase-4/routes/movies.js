165524d459c926254cea057f86a1370cd5daa49d
Abraham Galue <abrahamgalue512@gmail.com>
2024-08-01 21:33:05 -0400

    Separate logic from '/movies' routes and middlewares

---
+++ clase-4/routes/movies.js
@@ -0,0 +1,86 @@
+import { Router } from 'express'
+import { readJSON } from '../utils.js'
+import { validateMovie, validatePartialMovie } from '../schemas/movies.js'
+import { randomUUID } from 'node:crypto'
+
+const movies = readJSON('./movies.json')
+
+export const moviesRouter = Router()
+
+moviesRouter.get('/', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase()))
+
+    return res.json(filteredMovies)
+  }
+  res.json(movies)
+})
+
+moviesRouter.get('/:id', (req, res) => {
+  const { id } = req.params
+
+  const movie = movies.find(movie => movie.id === id)
+
+  if (movie) return res.json(movie)
+
+  res.status(404).json({ message: 'Movie not found' })
+})
+
+moviesRouter.post('/', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (!result.success) {
+    // 422 Unprocessable Entity
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  // en base de datos
+  const newMovie = {
+    id: randomUUID(), // uuid v4
+    ...result.data // ❌ req.body
+  }
+
+  // Esto no seria REST, porque estamos guardando
+  // el estado de la aplicacion en memoria
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie) // actualizar la cache del cliente
+})
+
+moviesRouter.delete('/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex < 0) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
+})
+
+moviesRouter.patch('/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) {
+    res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex < 0) {
+    res.status(404).json({ message: 'Movie not found' })
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+
+  res.json(updateMovie)
+})
--- clase-4/utils.js
+++ clase-4/utils.js
@@ -1,3 +1,10 @@
+// EN EL FUTURO: el import del json va a ser asi:
+// import movies from './movies.json' with { type: 'json' }
+
+// Como leer un json en ESModules
+// import fs from 'node:fs'
+// const movies = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'))
+
 import { createRequire } from 'node:module'
 const require = createRequire(import.meta.url)
 export const readJSON = path => require(path)
