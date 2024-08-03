bc62ab18bb2a997b65c1533c4b218f2982b8a1ee
Abraham Galue <abrahamgalue512@gmail.com>
2024-08-02 20:34:32 -0400

    Added MVC architecture pattern controller

--- clase-4/routes/movies.js
+++ clase-4/routes/movies.js
@@ -1,65 +1,10 @@
 import { Router } from 'express'
-import { validateMovie, validatePartialMovie } from '../schemas/movies.js'
-import { MovieModel } from '../models/movie.js'
+import { MovieController } from '../controllers/movies.js'
 
 export const moviesRouter = Router()
 
-moviesRouter.get('/', async (req, res) => {
-  const { genre } = req.query
-
-  const movies = await MovieModel.getAll({ genre })
-
-  res.json(movies)
-})
-
-moviesRouter.get('/:id', async (req, res) => {
-  const { id } = req.params
-
-  const movie = await MovieModel.getById({ id })
-
-  if (movie) return res.json(movie)
-
-  res.status(404).json({ message: 'Movie not found' })
-})
-
-moviesRouter.post('/', async (req, res) => {
-  const result = validateMovie(req.body)
-
-  if (!result.success) {
-    // 422 Unprocessable Entity
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const newMovie = await MovieModel.create({ input: result.data })
-
-  res.status(201).json(newMovie) // actualizar la cache del cliente
-})
-
-moviesRouter.delete('/:id', async (req, res) => {
-  const { id } = req.params
-
-  const result = await MovieModel.delete({ id })
-
-  if (!result) {
-    return res.status(404).json({ message: 'Movie not found' })
-  }
-
-  return res.json({ message: 'Movie deleted' })
-})
-
-moviesRouter.patch('/:id', async (req, res) => {
-  const result = validatePartialMovie(req.body)
-
-  if (!result.success) {
-    res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const { id } = req.params
-  const updatedMovie = await MovieModel.update({ id, input: result.data })
-
-  if (!updatedMovie) {
-    res.status(404).json({ message: 'Movie not found' })
-  }
-
-  res.json(updatedMovie)
-})
+moviesRouter.get('/', MovieController.getAll)
+moviesRouter.get('/:id', MovieController.getById)
+moviesRouter.post('/', MovieController.create)
+moviesRouter.delete('/:id', MovieController.delete)
+moviesRouter.patch('/:id', MovieController.update)
