340f4f6277f52aed9ab9dad6023d077e6cfba4d4
Abraham Galue <abrahamgalue512@gmail.com>
2024-08-09 13:41:33 -0400

    HTML file served to the client

--- clase-6/server/index.js
+++ clase-6/server/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT ?? 3000
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Chat en tiempo real</h1>')
+  res.sendFile(process.cwd() + '/client/index.html')
 })
 
 app.listen(port, () => {
