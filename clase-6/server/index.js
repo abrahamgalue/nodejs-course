874167b67797bcbb96de8bdbc2111b455cdcb1b3
Abraham Galue <abrahamgalue512@gmail.com>
2024-08-09 16:17:27 -0400

    WebSocket implementation created

--- clase-6/server/index.js
+++ clase-6/server/index.js
@@ -1,15 +1,28 @@
 import express from 'express'
 import logger from 'morgan'
 
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
 const port = process.env.PORT ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', client => {
+  console.log('a user has connected!')
+  client.on('disconnect', () => {
+    console.log('a user has disconnected')
+  })
+})
+
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`server listening on port http://localhost:${port}`)
 })
