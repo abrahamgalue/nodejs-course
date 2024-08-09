b17e335fe3817cbe9864f14257f8f0c69f9c6fde
Abraham Galue <abrahamgalue512@gmail.com>
2024-08-09 13:47:35 -0400

    Morgan HTTP request logging middleware installed

--- clase-6/server/index.js
+++ clase-6/server/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import logger from 'morgan'
 
 const port = process.env.PORT ?? 3000
 
 const app = express()
+app.use(logger('dev'))
 
 app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/client/index.html')
