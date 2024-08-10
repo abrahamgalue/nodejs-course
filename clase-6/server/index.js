c31ae55c2a6e1e5e8285f1f5c6ab931b10f3d367
Abraham Galue <abrahamgalue512@gmail.com>
2024-08-10 16:55:43 -0400

    Added DB Turso for data persistence

--- clase-6/server/index.js
+++ clase-6/server/index.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import logger from 'morgan'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
 
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+
+dotenv.config()
 
 const port = process.env.PORT ?? 3000
 
@@ -12,15 +16,53 @@ const io = new Server(server, {
   connectionStateRecovery: {}
 })
 
-io.on('connection', socket => {
+const db = createClient({
+  url: 'libsql://enjoyed-fury-abrahamgalue.turso.io',
+  authToken: process.env.DB_TOKEN
+})
+
+await db.execute(`CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  content TEXT
+  )
+`)
+
+io.on('connection', async socket => {
   console.log('a user has connected!')
+
   socket.on('disconnect', () => {
     console.log('a user has disconnected')
   })
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg)
+  socket.on('chat message', async (msg) => {
+    let result
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO messages (content) VALUES (:msg)',
+        args: { msg }
+      })
+    } catch (e) {
+      console.error(e)
+      return
+    }
+
+    io.emit('chat message', msg, result.lastInsertRowid.toString())
   })
+
+  if (!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: 'SELECT id, content FROM messages WHERE id > ?',
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+
+      results.rows.forEach(row => {
+        socket.emit('chat message', row.content, row.id.toString())
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
 })
 
 app.use(logger('dev'))
