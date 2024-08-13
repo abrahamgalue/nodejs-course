95c862e01837e75d4b08ee482997213955bd2e53
Abraham Galue <abrahamgalue512@gmail.com>
2024-08-13 17:52:44 -0400

    Added GET and POST enpoints

--- clase-7/index.js
+++ clase-7/index.js
@@ -7,6 +7,21 @@ app.get('/', (req, res) => {
   res.send('<h1>Hola Abraham nodejs</h1>')
 })
 
+app.post('/login', (req, res) => {
+})
+
+app.post('/register', (req, res) => {
+
+})
+
+app.post('/logout', (req, res) => {
+
+})
+
+app.get('/protected', (req, res) => {
+
+})
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`)
 })
