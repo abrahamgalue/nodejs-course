72449a15e5aed1fa10895144b743fb8abfa2a6eb
Abraham Galue <abrahamgalue512@gmail.com>
2024-08-14 16:09:09 -0400

    Added user session with JWT and cookies

--- clase-7/config.js
+++ clase-7/config.js
@@ -1,4 +1,5 @@
 export const {
   PORT = 3000,
-  SALT_ROUNDS = 10
+  SALT_ROUNDS = 10,
+  SECRET_JWT_KEY = 'this-is-not-awesome-secret-key-tiene-que-ser-mucho-mas-largo-que-esto-y-es-muy-seguro'
 } = process.env
--- clase-7/index.js
+++ clase-7/index.js
@@ -1,14 +1,30 @@
 import express from 'express'
-import { PORT } from './config.js'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
+import { PORT, SECRET_JWT_KEY } from './config.js'
 import { UserRepository } from './user-repository.js'
 
 const app = express()
-app.use(express.json())
-
 app.set('view engine', 'ejs')
 
+app.use(express.json())
+app.use(cookieParser())
+
+app.use((req, res, next) => {
+  const token = req.cookies.access_token
+  req.session = { user: null }
+
+  try {
+    const data = jwt.verify(token, SECRET_JWT_KEY)
+    req.session.user = data
+  } catch { }
+
+  next() // -> seguir a la siguiente ruta o middleware
+})
+
 app.get('/', (req, res) => {
-  res.render('index')
+  const { user } = req.session
+  res.render('index', user)
 })
 
 app.post('/login', async (req, res) => {
@@ -16,7 +32,18 @@ app.post('/login', async (req, res) => {
 
   try {
     const user = await UserRepository.login({ username, password })
-    res.send({ user })
+    // Creamos el JWT
+    const token = jwt.sign({ id: user._id, username: user.username }, SECRET_JWT_KEY, {
+      expiresIn: '1h'
+    })
+    res
+      .cookie('access_token', token, {
+        httpOnly: true, // la cookie solo se puede acceder en el servidor
+        secure: process.env.NODE_ENV === 'production', // la cookie solo se puede acceder en https
+        sameSite: 'strict', // la cookie solo se puede acceder en el mismo dominio
+        maxAge: 1000 * 60 * 60 // la cookie solo tiene un tiempo de validez de 1 hora
+      })
+      .send({ user })
   } catch (e) {
     res.status(401).send(e.message)
   }
@@ -38,7 +65,10 @@ app.post('/logout', (req, res) => {
 })
 
 app.get('/protected', (req, res) => {
+  const { user } = req.session
+  if (!user) return res.status(403).send('Access not authorized')
 
+  res.render('protected', user)
 })
 
 app.listen(PORT, () => {
